842e1e5ad0ae3415174b61fcd74d69e1d6ffac85
brenda87 <79244433+brenda87@users.noreply.github.com>
2025-11-03 18:01:51 +0300

    Enhance security in selectWinner function
    Updated selectWinner function to enhance security against reentrancy and overflow risks.

--- src/PuppyRaffle.sol
+++ src/PuppyRaffle.sol
@@ -129,12 +129,16 @@ contract PuppyRaffle is ERC721, Ownable {
     function selectWinner() external {
         require(block.timestamp >= raffleStartTime + raffleDuration, "PuppyRaffle: Raffle not over");
         require(players.length >= 4, "PuppyRaffle: Need at least 4 players");
+        //audit reentrancy attack
+        //fixes: use chainlink vrf 
         uint256 winnerIndex =
             uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length;
         address winner = players[winnerIndex];
         uint256 totalAmountCollected = players.length * entranceFee;
         uint256 prizePool = (totalAmountCollected * 80) / 100;
         uint256 fee = (totalAmountCollected * 20) / 100;
+        //audit overflow risk
+        //fixes: use a newer solidity version, use a bigger uint type
         totalFees = totalFees + uint64(fee);
 
         uint256 tokenId = totalSupply();
